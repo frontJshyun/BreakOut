e8e723ab6a6ad21688d8708ec25fc1f4c5516064
frontJshyun <daymisono@gmail.com>
2021-03-30 09:04:21 +0900

    update

--- src/utils/ServiceUtils.ts
+++ src/utils/ServiceUtils.ts
@@ -1,7 +1,7 @@
 export default {
   getAxiosConfig() {
     return {
-      baseURL: `http://localhost:4000/`,
+      baseURL: `http://52.78.44.119:4000/`,
       crossdomain: true,
       timeout: 60000
     };
