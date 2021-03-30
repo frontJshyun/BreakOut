298c2141a0622177caa912bdbd8bab4494853baf
frontJshyun <daymisono@gmail.com>
2021-03-30 14:58:46 +0900

    update

--- src/utils/ServiceUtils.ts
+++ src/utils/ServiceUtils.ts
@@ -1,7 +1,7 @@
 export default {
   getAxiosConfig() {
     return {
-      baseURL: `http://52.78.44.119:4000/`,
+      baseURL: `https://api.makeyourownimage.com/`,
       crossdomain: true,
       timeout: 60000
     };
